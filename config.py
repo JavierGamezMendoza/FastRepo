7a5898be98909c5fb78279d054f5302e56596f92
Habisito <javigm1012@gmail.com>
2022-11-10 22:23:38 +0100

    alpha 0.02

--- config.py
+++ config.py
@@ -1,5 +1,19 @@
 class User:
-    # Please change the fields to your own info
+    # Please change the fields to your owninfo
     userName="yourName"
     userMail="yourmail@example.com"
     gitToken="yourToken"
+
+
+
+class Colors:
+    # DONT CHANGE (if you dont like errors)
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
--- fastrepo.py
+++ fastrepo.py
@@ -1,7 +1,8 @@
 import argparse
 import sys
 import os
-from config import User
+import shutil
+from config import User, Colors as c
 from git import Repo
 from github import Github
 
@@ -14,12 +15,15 @@ parser = argparse.ArgumentParser(description=version,
 parser.add_argument("-v", "--version", action="store_true", help="Shows project version")
 subparsers = parser.add_subparsers(dest="command")
 
+#NEW
 parserNew = subparsers.add_parser("new", help="Add/Create repositories.")
 parserNew.add_argument("-l", "--local", type=(str), dest="PATH", help="Creates a local git repository on the gived path")
 parserNew.add_argument("-r", "--remote", type=(str), dest="REPONAME", help="Create a GitHub repository with the given name ")
 parserNew.add_argument("-p", "--private", type=(bool), dest="PRIVATE", default=False,  help="Set to true if you want your Github Repo Private. ")
-
+#SHOW
 parserShow = subparsers.add_parser("show", help="Show existing repositories") #TODO
+parserShow.add_argument("-r", "--remote", type=(str), dest="REPONAME", const="", nargs="?", help="Show the GitHub repositories of your account. ")
+#REMOVE
 parserRemove = subparsers.add_parser("remove", help="Remove existing repositories") #TODO
 
 args = parser.parse_args()
@@ -27,39 +31,75 @@ args = parser.parse_args()
 
 # CREATE REPO // NEW --LOCAL
 def createLocalRepo(path):
-    with Repo.config_writer() as git_config:
+    gitPath = path + "/.git"
+    if os.path.exists(gitPath):
+        print(c.WARNING + "There is a .git folder in that path...")
+        restart = input("Do you want to restart the repository? y/n: " + c.ENDC).lower()
+        if restart == "y":
+            shutil.rmtree(gitPath)
+            repo = Repo.init(path)
+            print("Repository has been deleted...")
+            print(c.OKGREEN + "New repository created." + c.ENDC)
+        else:
+            print("Directory NOT deleted")
+            repo = Repo(gitPath)
+    else:
+        Repo.init(gitPath)
+    
+    with repo.config_writer() as git_config:
             git_config.set_value('user', 'email', User.userMail)
             git_config.set_value('user', 'name', User.userName)
-    if os.path.exists(path, "/.git"):
-        print("There is a .git folder in that path...")
-        restart = input("Do you want to restart the repository? Y/n")
-        if restart:
-            os.remove(path, "/.git")
-    else:
-        newRepo = Repo.init(path, "/.git")
     return
 
 #CREATE GITHUB REPO // NEW --REMOTE
 def createRemoteRepo(repoName, private):
-    github = Github(User.gitToken)  
-    user = github.get_user(User.userName)
-    authed = github.get_user()
-    print(private)
-    authed.create_repo( repoName,
-                        description="An example of description",
-                        has_issues=False,
-                        has_projects=False,
-                        has_wiki=False,
-                        private=private
-                      )  
-    
+    try:
+        github = Github(User.gitToken)  
+        github.get_user(User.userName)
+        authed = github.get_user()
+        authed.create_repo( repoName,
+                            description="An example of description",
+                            has_issues=False,
+                            has_projects=False,
+                            has_wiki=False,
+                            private=private
+                        )  
+        print(c.OKGREEN + "Repositorio created correctly." + c.ENDC)
+    except: print(c.FAIL + "Something was wrong... Make sure of your config file and try again." + c.ENDC)
 
+#SHOW  
+def showRemoteRepo(repoName):
+    github = Github(User.gitToken) 
+    authed = github.get_user()
+    if repoName == "":
+        for repo in authed.get_repos():
+            print(repo.name)
+    else:
+        try:
+            repo = authed.get_repo(repoName)
+            print(repo.name)
+            print("--------------------------")
+            print(c.UNDERLINE + c.WARNING + "COMMITS \n" + c.ENDC)
+            for commit in repo.get_commits():
+                print(c.OKGREEN + str(commit) + c.ENDC)
+                print(commit.commit.author.date)
+                print("--------------------------")
+            print(c.UNDERLINE + c.WARNING + "BRANCHES \n" + c.ENDC)
+            for branch in repo.get_branches():
+                print (c.OKGREEN + str(branch) + c.ENDC)
+                print (branch.commit)
+                print("--------------------------")
+        except:
+            print(c.FAIL + "Some error ocurred... Try again." + c.ENDC)
+
+
+
+########## MAIN ############
+
+# SHOWS HELP IF NO ARGUMENTS
 if len(sys.argv) < 2:
     parser.print_help()
     sys.exit(1)
-
-# MAIN
-
 if args.version:
     print(version)
 if args.command == "new":
@@ -70,5 +110,8 @@ if args.command == "new":
         repoName = args.REPONAME
         print(repoName)
         private = args.PRIVATE
-
         createRemoteRepo(repoName, private)
+elif args.command == "show":
+    repoName = args.REPONAME
+    if hasattr(args, "REPONAME"):
+        showRemoteRepo(repoName)
